3498da7b1fab7699084eeff324a0f1c90523787b
Mdhasim-tech <131744384+Mdhasim-tech@users.noreply.github.com>
2023-04-29 12:29:38 +0530

    updated

--- drink_water_notification.py
+++ drink_water_notification.py
@@ -1,12 +1,13 @@
 
-
+import time
 from plyer import notification
 
 if __name__=="__main__":
-
-    notification.notify(title="**Please Drink Water Now!!",
-                        message="Health should be your first priority!!Health experts commonly recommend eight 8-ounce glasses, which equals about 2 liters, or half a gallon a day",
-                        app_icon=None,
-                        timeout=10)
+    while True:
+        notification.notify(title="**Please Drink Water Now!!",
+                            message="Health should be your first priority!!Health experts commonly recommend eight 8-ounce glasses, which equals about 2 liters, or half a gallon a day",
+                            app_icon=None,
+                            timeout=10)
+        time.sleep(180)
 
 
